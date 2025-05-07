54e4e20e281780576905fe3262604f1fcb9bdbcd
amiralimi <amirali.mirian@gmail.com>
2025-05-07 14:07:39 -0400

    adding tests for both compressed and uncompressed versions + some test refactoring.

--- tests/test_intersection_union.cu
+++ tests/test_intersection_union.cu
@@ -35,28 +35,47 @@ void calculate_intersection_union(
     }
 }
 
+#define COMPRESS_AND_CALCULATE(a, compressed, n_rows, n_cols, window_size, intersections, unions)       \
+    compress_1bit(a, compressed, n_rows, n_cols);                                                       \
+    fill_intersection_union(compressed, n_rows, n_cols / 32, window_size, intersections, unions);
+
+
 void test_intersection_union_kernel(int rows, int columns, int window_size, int seed, bool only_bench = false)
 {
+    std::cout << "test_intersection_union_kernel(rows=" << rows
+              << ", columns=" << columns
+              << ", window_size=" << window_size
+              << ", seed=" << seed << ") ... \n";
     std::vector<int> h_in = make_random_matrix<int>(rows * columns, [](int x) { return (int)(x % 2); }, seed);
 
     int *d_in = h2d(h_in);
 
     float *intersections;
     float *unions;
+    float *intersections_compressed;
+    float *unions_compressed;
     CHECK_CUDA(cudaMalloc(&intersections, rows * window_size * sizeof(float)));
     CHECK_CUDA(cudaMalloc(&unions, rows * window_size * sizeof(float)));
+    CHECK_CUDA(cudaMalloc(&intersections_compressed, rows * window_size * sizeof(float)));
+    CHECK_CUDA(cudaMalloc(&unions_compressed, rows * window_size * sizeof(float)));
+
+    unsigned int *compressed;
+    CHECK_CUDA(cudaMalloc(&compressed, rows * (columns / 32) * sizeof(unsigned int)));
 
     BENCH(fill_intersection_union(d_in, rows, columns, window_size, intersections, unions));
+    BENCH(
+        COMPRESS_AND_CALCULATE(d_in, compressed, rows, columns, window_size, intersections_compressed, unions_compressed)
+    );
 
     if (only_bench)
     {
-        std::cout << "test_intersection_union_kernel(rows=" << rows
-                  << ", columns=" << columns
-                  << ", window_size=" << window_size
-                  << ", seed=" << seed << ") Benchmarked\n";
         cudaFree(d_in);
         cudaFree(intersections);
         cudaFree(unions);
+        cudaFree(intersections_compressed);
+        cudaFree(unions_compressed);
+        cudaFree(compressed);
+        std::cout << "Benchmarked\n";
         return;
     }
     
@@ -69,10 +88,7 @@ void test_intersection_union_kernel(int rows, int columns, int window_size, int 
     assert_allclose(intersections, ref_intersections, 1e-5, "Intersections");
     assert_allclose(unions, ref_unions, 1e-5, "Unions");
 
-    std::cout << "test_intersection_union_kernel(rows=" << rows
-              << ", columns=" << columns
-              << ", window_size=" << window_size
-              << ", seed=" << seed << ") ✔︎\n";
+    std::cout << "Passed\n";
     cudaFree(d_in);
     cudaFree(intersections);
     cudaFree(unions);
@@ -92,7 +108,6 @@ void test_intersection_union(int seed = 42)
     test_intersection_union_kernel(128, 128, 128, seed);
     test_intersection_union_kernel(256, 256, 128, seed);
     test_intersection_union_kernel(16384, 28672, 64, seed, true);
-    test_intersection_union_kernel(16384, 28672, 128, seed, true);;
-    test_intersection_union_kernel(16384, 28672, 256, seed, true);;
-    // test_intersection_union_kernel(seed);
+    test_intersection_union_kernel(16384, 28672, 128, seed, true);
+    test_intersection_union_kernel(16384, 28672, 256, seed, true);
 }
